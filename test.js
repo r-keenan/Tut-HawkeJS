bb73d37517b6b4b9961e28d0615d3981c2a3f65e
r-keenan <ross.keenan@protonmail.com>
2021-04-18 20:06:31 -0500

    Added string templating

--- test.js
+++ test.js
@@ -3,10 +3,15 @@ let lastName = "Keenan";
 
 let myAge = 35;
 
+
 // alert(`My name is ${myName}.`);
 
 // alert(typeof myName);
 
 //alert(myAge);
 
-alert(firstName + " " + lastName);
+//alert(firstName + " " + lastName);
+
+alert(`${firstName} ${lastName}`);
+
+

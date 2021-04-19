384615abdec12e1e428ba5e59f0b84e8cb475039
r-keenan <ross.keenan@protonmail.com>
2021-04-18 20:16:40 -0500

    Added in parse method

--- test.js
+++ test.js
@@ -3,6 +3,21 @@ let lastName = "Keenan";
 
 let myAge = 35;
 
+let myBoolVal = true;
+
+//if (myBoolVal == true) {
+//  alert("The value is true. ");
+//} else {
+//  alert("It's not true. ");
+//}
+
+let floatNumber = 1.5;
+
+let myStringNum = "10";
+
+myStringNum = Number.parseInt(myStringNum);
+
+alert(myStringNum + floatNumber);
 
 // alert(`My name is ${myName}.`);
 
@@ -12,6 +27,4 @@ let myAge = 35;
 
 //alert(firstName + " " + lastName);
 
-alert(`${firstName} ${lastName}`);
-
-
+//alert(`${firstName} ${lastName}`);

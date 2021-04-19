a82b0e7568c6b3cbb945db38ba6f8a10c3204362
r-keenan <ross.keenan@protonmail.com>
2021-04-18 20:02:18 -0500

    Intial commit.

---
+++ test.js
@@ -0,0 +1,12 @@
+let firstName = "Ross";
+let lastName = "Keenan";
+
+let myAge = 35;
+
+// alert(`My name is ${myName}.`);
+
+// alert(typeof myName);
+
+//alert(myAge);
+
+alert(firstName + " " + lastName);

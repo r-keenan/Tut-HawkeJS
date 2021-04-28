4ab5f4693c2f261d6b756388983b89787ba5f8d5
r-keenan <ross.keenan@protonmail.com>
2021-04-27 19:59:22 -0500

    Added in turnary operators and case statement.

--- test.js
+++ test.js
@@ -1,32 +1,74 @@
-let firstName = "Ross";
-let lastName = "Keenan";
+// let firstName = "Ross";
+// let lastName = "Keenan";
 
-let myAge = 35;
+// let myAge = 35;
 
-let myBoolVal = true;
+// let myBoolVal = true;
 
-//if (myBoolVal == true) {
-//  alert("The value is true. ");
-//} else {
-//  alert("It's not true. ");
-//}
+// //if (myBoolVal == true) {
+// //  alert("The value is true. ");
+// //} else {
+// //  alert("It's not true. ");
+// //}
 
-let floatNumber = "1.5";
+// let floatNumber = "1.5";
 
-let myStringNum = "10";
+// let myStringNum = "10";
 
-myStringNum = Number.parseInt(myStringNum);
+// myStringNum = Number.parseInt(myStringNum);
 
-floatNumber = Number.parseFloat(floatNumber);
+// floatNumber = Number.parseFloat(floatNumber);
 
-alert(myStringNum + floatNumber);
+// alert(myStringNum + floatNumber);
 
-// alert(`My name is ${myName}.`);
+// // alert(`My name is ${myName}.`);
 
-// alert(typeof myName);
+// // alert(typeof myName);
 
-//alert(myAge);
+// //alert(myAge);
 
-//alert(firstName + " " + lastName);
+// //alert(firstName + " " + lastName);
 
-//alert(`${firstName} ${lastName}`);
+// //alert(`${firstName} ${lastName}`);
+
+// true = true
+// 1 = true
+//"aajskjlf;aj" = true
+
+//false = false
+// 0 = false
+// "" = false
+
+// let condition = "1";
+// ///=== this matches data type and value of the variable
+// if (condition === 1) {
+//   alert("this variable is true");
+// } else {
+//   alert("This variable is false");
+// }
+
+// //turnary operator
+
+// condition === "1" ? alert("This is true.") : alert("This is false.");
+
+// let fruit = "apple";
+
+// fruit === "orange"
+//   ? alert("orange")
+//   : fruit === "apple"
+//   ? alert("apple")
+//   : "fruit is not listed";
+
+// //switch statement
+
+// switch (fruit) {
+//   case "apple":
+//     alert("Apple!");
+//     break;
+//   case "orange":
+//     alert("Orange!");
+//     break;
+//   default:
+//     alert("Fruit is not found");
+//     break;
+// }

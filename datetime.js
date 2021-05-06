c768964e8d501a6424c022f350c74bbf383e907a
r-keenan <ross.keenan@protonmail.com>
2021-05-06 18:51:05 -0500

    broke out datetime to its own file

---
+++ datetime.js
@@ -0,0 +1,9 @@
+// let myHeader = document.querySelector("#header");
+
+// myHeader.innerHTML = "<span style='color: red'>we</span> changed the text";
+
+let date2 = new Date();
+
+let date = Date.now();
+
+alert(date2);
--- test.js
+++
@@ -1,7 +0,0 @@
-// let myHeader = document.querySelector("#header");
-
-// myHeader.innerHTML = "<span style='color: red'>we</span> changed the text";
-
-let date = Date.now();
-
-alert(date);

2842b0c1b2daa3657e44833a4a80aff5a9041bf5
r-keenan <ross.keenan@protonmail.com>
2021-05-06 19:28:43 -0500

    Broke out consoleMessages to its own file.

---
+++ consoleMessages.js
@@ -0,0 +1,6 @@
+console.log("this is a console log.");
+console.debug("this is a debugging message.");
+
+console.warn("this is a warning message. This will soon be deprecated");
+
+console.error("This is not going to work. It is broken!");
